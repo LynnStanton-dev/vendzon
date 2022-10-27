225348b41008bd2bf1e441e6ec40eddfbcf70768
LynnStanton-dev <trndurp@gmail.com>
2022-10-27 16:48:33 +0200

    as of 27/10/2022 after school

--- src/App.js
+++ src/App.js
@@ -2,8 +2,19 @@ import Navbar from "./components/Navbar"
 import Explore from "./components/Explore"
 import Cart from "./components/Cart"
 import { Routes, Route, Link } from "react-router-dom"
+import { useState } from "react"
 
 function App() {
+  const [products, setProducts] = useState([
+    {
+      id: 0,
+      title: "Harman Kardon Onyx Studio 4",
+      img: "https://unsplash.it/500/500",
+      soldOut: false,
+      inSlideshow: true
+    },
+  ])
+
   return (
     <div className="App">
       <Navbar />
@@ -14,7 +25,7 @@ function App() {
             <Link to="/">Back home</Link>
           </>
         } />
-        <Route path="/" element={<Explore />} />
+        <Route path="/" element={<Explore products={products} setProducts={setProducts} />} />
         <Route path="/cart" element={<Cart />} />
       </Routes>
     </div>
--- src/components/Explore.js
+++ src/components/Explore.js
@@ -1,12 +1,33 @@
+import { useState, useEffect } from "react"
 
-export default function Explore() {
+export default function Explore(props) {
+    const { products } = props
+    const slideshow = products.filter(product => product.inSlideshow && product.soldOut)
+    const [curr, setCurr] = useState(0)
+
+    useEffect(
+        () => setInterval(
+            () => {
+                try {
+                    setCurr(curr + 1)
+                } catch (err) {
+                    setCurr(0)
+                }
+            }, 10000
+        ), [curr]
+    )
+
     return (
         <div className="explore">
+            <div>
+                {products.map(product => <li key={product.id}>{product.title}</li>)}
+            </div>
             <div className="slideshow">
                 <button className="prev">&lt;</button>
                 <button className="next">&gt;</button>
-                <div>
-
+                <div className="background">
+                    <img src={slideshow[curr].img} alt={slideshow[curr].title} />
+                    {slideshow[curr].title}
                 </div>
             </div>
         </div>
